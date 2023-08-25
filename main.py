b8bbde24f6b527a9bb65de1c3c9c5fdc0dad0070
jay1224-jay <wuj20061224@gmail.com>
2023-08-25 13:24:15 +0800

    initialization

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import ui
+
+
+ui.generate_ui(st)
+
+st.write("ui.py work successfully")
---
+++ ui.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def generate_ui(root):
+    root.sidebar.write("hello from sidebar")
+    root.sidebar.write("hello from sidebar")
+
